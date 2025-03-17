f9287294b432e18e81d603cd30670da4b147e1af
jane00 <305020942@qq.com>
2025-03-17 12:58:51 +0800

    Create predownload.sh

---
+++ predownload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/jane00/Termux_XFCE_Qualcomm/raw/main/mesa-vulkan-kgsl_23.3.0-ubuntu_arm64.deb
+wget https://github.com/jane00/Termux_XFCE_Qualcomm/raw/main/termux-x11.deb
+wget https://github.com/jane00/Termux_XFCE_Qualcomm/raw/main/termux-x11.apk
+
